ce77530f36fbcacc2e549ef9da5caa465a1b5884
elaine01 <ecumming.g@gmail.com>
2018-02-04 14:54:34 -0800

    removed keys from gitignore

---
+++ keys.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+	consumer_key: "HFt10iZZOA3cuqPgtmh2dPCjH",
+	consumer_secret: "ilEZrzuC6K3pVkp9YL8OhCoOb2YnHGLVJ1YCV0BZveqsTP0Qx1",
+	access_token_key: "954812563824377857-asPygZLcfQYeUJXiQfcLibOS90cLPag",
+	access_token_secret: "PROxiJVHcAQzrpytKbYn7xc8CvwKeIKuB5nHcqNaAsgi3",
+}; 
+
+module.exports = twitterKeys;
